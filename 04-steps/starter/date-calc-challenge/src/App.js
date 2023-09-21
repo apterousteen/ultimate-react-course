e9295e37874096baf3b0ce6e621769812d74c99a
apterousteen <ayzhan312@live.ru>
2023-09-21 18:24:34 +0500

    data calc challenge_2 completed

--- 04-steps/starter/date-calc-challenge/src/App.js
+++ 04-steps/starter/date-calc-challenge/src/App.js
@@ -24,24 +24,66 @@ function Counter() {
       return `${-count} ${count === 1 ? 'day' : 'days'} ago was `;
   };
 
+  const reset = () => {
+    setStep(1);
+    setCount(0);
+  };
+
   return (
     <>
       <div>
-        <button onClick={() => (step > 1 ? setStep((s) => s - 1) : null)}>
-          -
-        </button>
-        <span>Step: {step}</span>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
+        <span style={{ marginRight: '0.5rem' }}>Step: {step}</span>
+        <input
+          type="range"
+          min="1"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(+e.target.value)}
+        />
       </div>
+
       <div>
+        <span style={{ marginRight: '0.5rem' }}>Count:</span>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count: {count}</span>
+        <input
+          value={count}
+          onChange={(e) =>
+            Number.isFinite(+e.target.value) ? setCount(+e.target.value) : null
+          }
+        />
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
+
       <p>
         {getDayString()}
         {date.toDateString()}
       </p>
+
+      {count !== 0 || step !== 1 ? (
+        <button onClick={reset}>RESET</button>
+      ) : null}
     </>
   );
+  // Old version
+
+  // return (
+  //   <>
+  //     <div>
+  //       <button onClick={() => (step > 1 ? setStep((s) => s - 1) : null)}>
+  //         -
+  //       </button>
+  //       <span>Step: {step}</span>
+  //       <button onClick={() => setStep((s) => s + 1)}>+</button>
+  //     </div>
+  //     <div>
+  //       <button onClick={() => setCount((c) => c - step)}>-</button>
+  //       <span>Count: {count}</span>
+  //       <button onClick={() => setCount((c) => c + step)}>+</button>
+  //     </div>
+  //     <p>
+  //       {getDayString()}
+  //       {date.toDateString()}
+  //     </p>
+  //   </>
+  // );
 }
