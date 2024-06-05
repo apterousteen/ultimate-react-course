cd58ca0e9693b7a2a90cf29a9d60461754d8cbab
apterousteen <ayzhan312@live.ru>
2024-06-05 23:40:27 +0500

    feat: React Quiz raw app structure

--- 10-react-quiz/react-quiz/src/App.js
+++ 10-react-quiz/react-quiz/src/App.js
@@ -1,9 +1,15 @@
-import DateCounter from './DateCounter';
+import Header from './components/Header';
+import Main from './components/Main';
 
 export default function App() {
   return (
-    <>
-      <DateCounter></DateCounter>
-    </>
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+        <progress></progress>
+      </Main>
+    </div>
   );
 }
